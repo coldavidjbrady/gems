aaf846a68eb7d126cfcda5068e536287713ab387
davidbrady <coldavidjbrady@gmail.com>
2016-02-07 13:47:02 -0800

    Added $http dependency injection

--- products.js
+++ products.js
@@ -2,31 +2,30 @@ var app = angular.module('store-directives', []);
 
 app.directive("descriptions", function() {
   return {
-    restrict: "E",
-    templateUrl: "descriptions.html"
+      restrict: "E",
+      templateUrl: "descriptions.html"
   };
 });
 
 app.directive("reviews", function() {
   return {
-    restrict: "E",
-    templateUrl: "reviews.html"
+      restrict: "E",
+      templateUrl: "reviews.html"
   };
 });
 
 app.directive("specs", function() {
   return {
-    restrict: "E",
-    templateUrl: "specs.html"
+      restrict: "E",
+      templateUrl: "specs.html"
   };
 });
 
 app.directive("productTabs", function() {
   return {
-    restrict: "E",
-
-    templateUrl: "product-tabs.html",
-    controller: function() {
+      restrict: "E",
+      templateUrl: "product-tabs.html",
+      controller: function() {
       this.tab = 1;
 
       this.isSet = function(checkTab) {
