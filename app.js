3b190e0d1338aaec536371cb007df0a9a9c6d6a3
davidbrady <coldavidjbrady@gmail.com>
2017-08-27 14:16:34 -0700

    Removed PanelController because it is redundant with the existing productTabs directive

--- app.js
+++ app.js
@@ -64,17 +64,4 @@
                 this.review = {};
             }
         }]);
-
-        app.controller("PanelController", function() {
-	        this.tab = 1;
-	  
-	        this.selectTab = function(setTab) {
-		        this.tab = setTab;
-	        };
-	  
-	        this.isSelected = function(checkTab) {
-		        return this.tab === checkTab;
-	        };
-        });
-
 })();
